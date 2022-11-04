486c26952d54ecd8bf2d00d739c40b32edbf2d09
BloodDragon2580 <gewinner.thomas@gmail.com>
2022-11-04 22:59:07 +0100

    Update to v8.1
    Fix for the overlay error when making the window larger or smaller.

--- Portable/Main.lua
+++ Portable/Main.lua
@@ -696,37 +696,32 @@ function me:UpdateUI_ButtonGrid()
 		me:Helper_AdjustFont(me.ui[button].text.name, me.db.profile.textFont, me.db.profile.textSize, me.db.profile.textFlags)
 	end
 	
-	-- Priority (LEFT) 优先 (左)
+	-- Priority (LEFT)
 	if (me.db.profile.iconLayout == 1) then
 		me.ui.button1:SetPoint("TOPLEFT", me.ui.container, "TOPLEFT",  pad, -pad)
 		me.ui.button1:SetSize(size * 2 + space + space, size * 2 + space + space)
 		me.ui.button1.text.name:ClearAllPoints()
 		me.ui.button1.text.name:SetPoint(me.db.profile.textPos, me.ui.button1.text, me.db.profile.textPos, me.db.profile.textOffX * 2, me.db.profile.textOffY * 2)
 		me:Helper_AdjustFont(me.ui.button1.text.name, me.db.profile.textFont, me.db.profile.textSize * 2, me.db.profile.textFlags)
-		
+		--Line 1
 		me.ui.button2:SetPoint("TOPLEFT", me.ui.button1, "TOPRIGHT", space, 0)
-		--me.ui.button2:SetSize(size * 1 + space, size * 1 + space)
-		--me.ui.button2.text.name:ClearAllPoints()
-		--me.ui.button2.text.name:SetPoint(me.db.profile.textPos, me.ui.button2.text, me.db.profile.textPos, me.db.profile.textOffX * 1, me.db.profile.textOffY * 1)
-		--me:Helper_AdjustFont(me.ui.button2.text.name, me.db.profile.textFont, me.db.profile.textSize * 1, me.db.profile.textFlags)
-		
 		me.ui.button3:SetPoint("TOPLEFT", me.ui.button2, "TOPRIGHT", space, 0)
 		me.ui.button4:SetPoint("TOPLEFT", me.ui.button3, "TOPRIGHT", space, 0)
 		me.ui.button5:SetPoint("TOPLEFT", me.ui.button4, "TOPRIGHT", space, 0)		
 		me.ui.button6:SetPoint("TOPLEFT", me.ui.button5, "TOPRIGHT", space, 0)
-		
-		me.ui.button7:SetPoint("BOTTOMLEFT", me.ui.button1, "BOTTOMRIGHT", space, -0)
-		me.ui.button8:SetPoint("BOTTOMLEFT", me.ui.button7, "BOTTOMRIGHT", space, 0)
-		me.ui.button9:SetPoint("BOTTOMLEFT", me.ui.button8, "BOTTOMRIGHT", space, 0)	
-		me.ui.button10:SetPoint("BOTTOMLEFT", me.ui.button9, "BOTTOMRIGHT", space, 0)	
-		me.ui.button11:SetPoint("BOTTOMLEFT", me.ui.button10, "BOTTOMRIGHT", space, 0)
-
-		me.ui.button12:SetPoint("BOTTOMLEFT", me.ui.button1, "BOTTOMLEFT", space, -100)
-		me.ui.button13:SetPoint("BOTTOMLEFT", me.ui.button12, "BOTTOMRIGHT", space, 0)
-		me.ui.button14:SetPoint("BOTTOMLEFT", me.ui.button13, "BOTTOMRIGHT", space, 0)	
-		me.ui.button15:SetPoint("BOTTOMLEFT", me.ui.button14, "BOTTOMRIGHT", space, 0)
-		me.ui.button16:SetPoint("BOTTOMLEFT", me.ui.button15, "BOTTOMRIGHT", space, 0)
-		me.ui.button17:SetPoint("BOTTOMLEFT", me.ui.button16, "BOTTOMRIGHT", space, 0)
+		me.ui.button7:SetPoint("TOPLEFT", me.ui.button6, "TOPRIGHT", space, 0)
+		--Line 2
+		me.ui.button8:SetPoint("BOTTOMLEFT", me.ui.button1, "BOTTOMRIGHT", space, 0)
+		me.ui.button9:SetPoint("TOPLEFT", me.ui.button8, "TOPRIGHT", space, 0)
+		me.ui.button10:SetPoint("TOPLEFT", me.ui.button9, "TOPRIGHT", space, 0)
+		me.ui.button11:SetPoint("TOPLEFT", me.ui.button10, "TOPRIGHT", space, 0)
+		me.ui.button12:SetPoint("TOPLEFT", me.ui.button11, "TOPRIGHT", space, 0)
+		me.ui.button13:SetPoint("TOPLEFT", me.ui.button12, "TOPRIGHT", space, 0)
+		--Line 3
+		me.ui.button14:SetPoint("BOTTOMLEFT", me.ui.container, "BOTTOMLEFT",  pad, -pad)
+		me.ui.button15:SetPoint("TOPLEFT", me.ui.button14, "TOPRIGHT", space, 0)
+		me.ui.button16:SetPoint("TOPLEFT", me.ui.button15, "TOPRIGHT", space, 0)
+		me.ui.button17:SetPoint("TOPLEFT", me.ui.button16, "TOPRIGHT", space, 0)
 
 	
 	-- Simple Rows
@@ -747,7 +742,7 @@ function me:UpdateUI_ButtonGrid()
 			end
 		end
 
-	-- Center of Attention 中间为主
+	-- Center of Attention
 	elseif (me.db.profile.iconLayout == 3) then
 		me.ui.button4:SetPoint("TOPLEFT", me.ui.container, "TOPLEFT", pad, -pad)
 		
@@ -778,101 +773,96 @@ function me:UpdateUI_ButtonGrid()
 		me.ui.button9:SetPoint("TOPLEFT", me.ui.button2, "BOTTOMLEFT", 0, -space)
 		me.ui.button10:SetPoint("TOPRIGHT", me.ui.button9, "BOTTOMRIGHT", 0, -space)
 		me.ui.button11:SetPoint("TOPLEFT", me.ui.button3, "BOTTOMLEFT", 0, -space)
-		me.ui.button12:SetPoint("TOPLEFT", me.ui.button3, "TOPLEFT", 200, -space)
-		me.ui.button13:SetPoint("TOPLEFT", me.ui.button7, "BOTTOMLEFT", 0, -space)
+		me.ui.button12:SetPoint("TOPLEFT", me.ui.button5, "TOPRIGHT", 0, -space)
+		me.ui.button13:SetPoint("TOPLEFT", me.ui.button11, "BOTTOMLEFT", 0, -space)
 		
 		me.ui.button14:SetPoint("TOPLEFT", me.ui.button2, "TOPRIGHT", 0, -space)	
 		me.ui.button15:SetPoint("TOPLEFT", me.ui.button2, "BOTTOMRIGHT", 0, -space)
 		me.ui.button16:SetPoint("TOPLEFT", me.ui.button9, "BOTTOMRIGHT", 0, -space)
 		me.ui.button17:SetPoint("TOPRIGHT", me.ui.button12, "BOTTOMRIGHT", 0, -space)
 		
-	-- Look at Me (LEFT) 超大! (左)
+	-- Look at Me (LEFT)
 	elseif (me.db.profile.iconLayout == 4) then
 		me.ui.button1:SetPoint("TOPLEFT", me.ui.container, "TOPLEFT",  pad, -pad)
 		me.ui.button1:SetSize(size * 4 + (space * 3), size * 4 + (space * 3))
 		me.ui.button1.text.name:ClearAllPoints()
 		me.ui.button1.text.name:SetPoint(me.db.profile.textPos, me.ui.button1.text, me.db.profile.textPos, me.db.profile.textOffX * 2.5, me.db.profile.textOffY * 2.5)
 		me:Helper_AdjustFont(me.ui.button1.text.name, me.db.profile.textFont, me.db.profile.textSize * 2.5, me.db.profile.textFlags)
-		
+		--Line 1
 		me.ui.button2:SetPoint("TOPLEFT", me.ui.button1, "TOPRIGHT", space, 0)
 		me.ui.button3:SetPoint("TOPLEFT", me.ui.button2, "BOTTOMLEFT", 0, -space)
 		me.ui.button4:SetPoint("TOPLEFT", me.ui.button3, "BOTTOMLEFT", 0, -space)
-		me.ui.button5:SetPoint("TOPLEFT", me.ui.button4, "BOTTOMLEFT", 0, -space)
-		
-		me.ui.button6:SetPoint("TOPLEFT", me.ui.button2, "TOPRIGHT", space, 1)
+		me.ui.button5:SetPoint("TOPLEFT", me.ui.button4, "BOTTOMLEFT", 0, -space)		
+		--Line 2
+		me.ui.button6:SetPoint("TOPLEFT", me.ui.button2, "TOPRIGHT", space, 0)
 		me.ui.button7:SetPoint("TOPLEFT", me.ui.button6, "BOTTOMLEFT", 0, -space)
 		me.ui.button8:SetPoint("TOPLEFT", me.ui.button7, "BOTTOMLEFT", 0, -space)
 		me.ui.button9:SetPoint("TOPLEFT", me.ui.button8, "BOTTOMLEFT", 0, -space)
-		
+		--Line 3
 		me.ui.button10:SetPoint("TOPLEFT", me.ui.button6, "TOPRIGHT", space, 0)
 		me.ui.button11:SetPoint("TOPLEFT", me.ui.button10, "BOTTOMLEFT", 0, -space)
 		me.ui.button12:SetPoint("TOPLEFT", me.ui.button11, "BOTTOMLEFT", 0, -space)
 		me.ui.button13:SetPoint("TOPLEFT", me.ui.button12, "BOTTOMLEFT", 0, -space)
+		--Line 4
+		me.ui.button14:SetPoint("TOPLEFT", me.ui.button10, "TOPRIGHT", space, 0)
+		me.ui.button15:SetPoint("TOPLEFT", me.ui.button14, "BOTTOMLEFT", 0, -space)
+		me.ui.button16:SetPoint("TOPLEFT", me.ui.button15, "BOTTOMLEFT", 0, -space)
+		me.ui.button17:SetPoint("TOPLEFT", me.ui.button16, "BOTTOMLEFT", 0, -space)
 		
-		me.ui.button14:SetPoint("TOPLEFT", me.ui.button10, "TOPRIGHT", 0, -space)
-		me.ui.button15:SetPoint("TOPLEFT", me.ui.button12, "TOPRIGHT", 0, -space)
-		me.ui.button16:SetPoint("TOPLEFT", me.ui.button13, "TOPRIGHT", 0, -space)
-		me.ui.button17:SetPoint("TOPRIGHT", me.ui.button14, "TOPRIGHT", 0, -100)
-		
-	-- Look at Me (RIGHT) 超大! (右)
+	-- Look at Me (RIGHT)
 	elseif (me.db.profile.iconLayout == 5) then
 		me.ui.button1:SetPoint("TOPRIGHT", me.ui.container, "TOPRIGHT",  -pad, -pad)
 		me.ui.button1:SetSize(size * 4 + (space * 3), size * 4 + (space * 3))
 		me.ui.button1.text.name:ClearAllPoints()
 		me.ui.button1.text.name:SetPoint(me.db.profile.textPos, me.ui.button1.text, me.db.profile.textPos, me.db.profile.textOffX * 2.5, me.db.profile.textOffY * 2.5)
 		me:Helper_AdjustFont(me.ui.button1.text.name, me.db.profile.textFont, me.db.profile.textSize * 2.5, me.db.profile.textFlags)
-		
+		--Line 1
 		me.ui.button2:SetPoint("TOPRIGHT", me.ui.button1, "TOPLEFT", -space, 0)
-		me.ui.button3:SetPoint("TOPLEFT", me.ui.button2, "BOTTOMLEFT", 0, -space)
-		me.ui.button4:SetPoint("TOPLEFT", me.ui.button3, "BOTTOMLEFT", 0, -space)
-		me.ui.button5:SetPoint("TOPLEFT", me.ui.button4, "BOTTOMLEFT", 0, -space)
-		
+		me.ui.button3:SetPoint("TOPRIGHT", me.ui.button2, "BOTTOMRIGHT", 0, -space)
+		me.ui.button4:SetPoint("TOPRIGHT", me.ui.button3, "BOTTOMRIGHT", 0, -space)
+		me.ui.button5:SetPoint("TOPRIGHT", me.ui.button4, "BOTTOMRIGHT", 0, -space)
+		--Line 2
 		me.ui.button6:SetPoint("TOPRIGHT", me.ui.button2, "TOPLEFT", -space, 0)
-		me.ui.button7:SetPoint("TOPLEFT", me.ui.button6, "BOTTOMLEFT", 0, -space)
-		me.ui.button8:SetPoint("TOPLEFT", me.ui.button7, "BOTTOMLEFT", 0, -space)
-		me.ui.button9:SetPoint("TOPLEFT", me.ui.button8, "BOTTOMLEFT", 0, -space)
-		
+		me.ui.button7:SetPoint("TOPRIGHT", me.ui.button6, "BOTTOMRIGHT", 0, -space)
+		me.ui.button8:SetPoint("TOPRIGHT", me.ui.button7, "BOTTOMRIGHT", 0, -space)
+		me.ui.button9:SetPoint("TOPRIGHT", me.ui.button8, "BOTTOMRIGHT", 0, -space)
+		--Line 3
 		me.ui.button10:SetPoint("TOPRIGHT", me.ui.button6, "TOPLEFT", -space, 0)
-		me.ui.button11:SetPoint("TOPLEFT", me.ui.button10, "BOTTOMLEFT", 0, -space)
-		me.ui.button12:SetPoint("TOPLEFT", me.ui.button11, "BOTTOMLEFT", 0, -space)
-		me.ui.button13:SetPoint("TOPLEFT", me.ui.button12, "BOTTOMLEFT", 0, -space)
-
+		me.ui.button11:SetPoint("TOPRIGHT", me.ui.button10, "BOTTOMRIGHT", 0, -space)
+		me.ui.button12:SetPoint("TOPRIGHT", me.ui.button11, "BOTTOMRIGHT", 0, -space)
+		me.ui.button13:SetPoint("TOPRIGHT", me.ui.button12, "BOTTOMRIGHT", 0, -space)
+		--Line 4
 		me.ui.button14:SetPoint("TOPRIGHT", me.ui.button10, "TOPLEFT", 0, -space)
-		me.ui.button15:SetPoint("TOPRIGHT", me.ui.button12, "TOPLEFT", 0, -space)
-		me.ui.button16:SetPoint("TOPRIGHT", me.ui.button13, "TOPLEFT", 0, -space)
-		me.ui.button17:SetPoint("TOPLEFT", me.ui.button14, "TOPLEFT", 0, -100)
+		me.ui.button15:SetPoint("TOPRIGHT", me.ui.button14, "BOTTOMRIGHT", 0, -space)
+		me.ui.button16:SetPoint("TOPRIGHT", me.ui.button15, "BOTTOMRIGHT", 0, -space)
+		me.ui.button17:SetPoint("TOPRIGHT", me.ui.button16, "BOTTOMRIGHT", 0, -space)
 		
-	-- Priority (RIGHT) 优先 (右) ... A few guildies have asked for a backwards Priority Layout, so here it is
+	-- Priority (RIGHT)... A few guildies have asked for a backwards Priority Layout, so here it is
 	elseif (me.db.profile.iconLayout == 6) then
 		me.ui.button1:SetPoint("TOPRIGHT", me.ui.container, "TOPRIGHT",  -pad, -pad)
 		me.ui.button1:SetSize(size * 2 + space + space, size * 2 + space + space)
 		me.ui.button1.text.name:ClearAllPoints()
 		me.ui.button1.text.name:SetPoint(me.db.profile.textPos, me.ui.button1.text, me.db.profile.textPos, me.db.profile.textOffX * 2, me.db.profile.textOffY * 2)
 		me:Helper_AdjustFont(me.ui.button1.text.name, me.db.profile.textFont, me.db.profile.textSize * 2, me.db.profile.textFlags)
-		
+		--Line 1
 		me.ui.button2:SetPoint("TOPRIGHT", me.ui.button1, "TOPLEFT", -space, 0)
-		--me.ui.button2:SetSize(size * 1 + space, size * 1 + space)
-		--me.ui.button2.text.name:ClearAllPoints()
-		--me.ui.button2.text.name:SetPoint(me.db.profile.textPos, me.ui.button2.text, me.db.profile.textPos, me.db.profile.textOffX * 1, me.db.profile.textOffY * 1)
-		--me:Helper_AdjustFont(me.ui.button2.text.name, me.db.profile.textFont, me.db.profile.textSize * 1, me.db.profile.textFlags)
-		
 		me.ui.button3:SetPoint("TOPRIGHT", me.ui.button2, "TOPLEFT", -space, 0)
 		me.ui.button4:SetPoint("TOPRIGHT", me.ui.button3, "TOPLEFT", -space, 0)
 		me.ui.button5:SetPoint("TOPRIGHT", me.ui.button4, "TOPLEFT", -space, 0)
-		me.ui.button6:SetPoint("TOPRIGHT", me.ui.button5, "TOPLEFT", -space, 0)
-		
-		me.ui.button7:SetPoint("BOTTOMRIGHT", me.ui.button1, "BOTTOMLEFT", -space, -0)
-		me.ui.button8:SetPoint("BOTTOMRIGHT", me.ui.button7, "BOTTOMLEFT", -space, 0)
-		me.ui.button9:SetPoint("BOTTOMRIGHT", me.ui.button8, "BOTTOMLEFT", -space, 0)
-		me.ui.button10:SetPoint("BOTTOMRIGHT", me.ui.button9, "BOTTOMLEFT", -space, 0)
-		me.ui.button11:SetPoint("BOTTOMRIGHT", me.ui.button10, "BOTTOMLEFT", -space, 0)
-	
-		me.ui.button12:SetPoint("BOTTOMRIGHT", me.ui.button1, "BOTTOMRIGHT", -space, -100)
-		me.ui.button13:SetPoint("BOTTOMRIGHT", me.ui.button12, "BOTTOMLEFT", -space, 0)
-		me.ui.button14:SetPoint("BOTTOMRIGHT", me.ui.button13, "BOTTOMLEFT", -space, 0)
-		me.ui.button15:SetPoint("BOTTOMRIGHT", me.ui.button14, "BOTTOMLEFT", -space, 0)
-		me.ui.button16:SetPoint("BOTTOMRIGHT", me.ui.button15, "BOTTOMLEFT", -space, 0)		
-		me.ui.button17:SetPoint("BOTTOMRIGHT", me.ui.button16, "BOTTOMLEFT", -space, 0)		
+		me.ui.button6:SetPoint("TOPRIGHT", me.ui.button5, "TOPLEFT", -space, 0)		
+		me.ui.button7:SetPoint("TOPRIGHT", me.ui.button6, "TOPLEFT", -space, -0)
+		--Line 2
+		me.ui.button8:SetPoint("BOTTOMRIGHT", me.ui.button1, "BOTTOMLEFT", -space, 0)
+		me.ui.button9:SetPoint("TOPRIGHT", me.ui.button8, "TOPLEFT", -space, 0)
+		me.ui.button10:SetPoint("TOPRIGHT", me.ui.button9, "TOPLEFT", -space, 0)
+		me.ui.button11:SetPoint("TOPRIGHT", me.ui.button10, "TOPLEFT", -space, 0)
+		me.ui.button12:SetPoint("TOPRIGHT", me.ui.button11, "TOPLEFT", -space, 0)
+		me.ui.button13:SetPoint("TOPRIGHT", me.ui.button12, "TOPLEFT", -space, 0)
+		--Line 3
+		me.ui.button14:SetPoint("BOTTOMRIGHT", me.ui.container, "BOTTOMRIGHT",  pad, -pad)
+		me.ui.button15:SetPoint("TOPRIGHT", me.ui.button14, "TOPLEFT", -space, 0)
+		me.ui.button16:SetPoint("TOPRIGHT", me.ui.button15, "TOPLEFT", -space, 0)		
+		me.ui.button17:SetPoint("TOPRIGHT", me.ui.button16, "TOPLEFT", -space, 0)	
 
 	-- Invalid
 	else
